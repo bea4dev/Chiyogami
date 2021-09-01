68dc34c37c67630f975c3c092f4c11ff3d485dda
Be4rJP <bea0224@outlook.jp>
2021-09-01 18:23:22 +0900

    add ChiyogamiLib fix WorldServer unsafe method

--- buildPatches.sh
+++ buildPatches.sh
@@ -1,7 +1,9 @@
 diff --strip-trailing-cr -urN Original/Paper-Server/src Paper-Server/src > server.patch
+diff --strip-trailing-cr -urN Original/Paper-Server/pom.xml Paper-Server/pom.xml > pom.xml.patch
 diff --strip-trailing-cr -urN Original/Paper-API/src Paper-API/src > api.patch
 diff --strip-trailing-cr -urN Original/Paper-MojangAPI/src Paper-MojangAPI/src > mojang.patch
 mkdir -p patches
 mv -f server.patch patches
+mv -f pom.xml.patch patches
 mv -f api.patch patches
 mv -f mojang.patch patches
