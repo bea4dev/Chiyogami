973451b6d4bf103109127aa1e6b11c8617ba4d1e
Be4rJP <bea0224@outlook.jp>
2022-05-05 00:06:42 +0900

    Chiyogami initial commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,6 @@ pluginManagement {
     }
 }
 
-rootProject.name = "forktest"
+rootProject.name = "chiyogami"
 
-include("forktest-api", "forktest-server")
+include("chiyogami-api", "chiyogami-server")
